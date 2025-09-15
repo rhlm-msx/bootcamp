21a56e4861d9a3bc239d1f71973ee7947594529d
Rahul Mishra <rahul.mishra@mindstix.com>
2025-09-15 09:47:41 +0000

    updated infra code

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,8 +1,8 @@
-resource "aws_ecr_repository" "test" {
+resource "aws_ecr_repository" "lambda_ecr" {
   name                 = "test"
   image_tag_mutability = "MUTABLE"
 }
 
 output "ecr_url" {
-  value = aws_ecr_repository.test.repository_url
+  value = aws_ecr_repository.lambda_ecr.repository_url
 }
--- infra/lambda.tf
+++ infra/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "lambda" {
 	function_name = "lambda"
 	role = aws_iam_role.lambda_role.arn
 	package_type = "Image"
-	image_uri = "${aws_ecr_repository.test.repository_url}:latest"
+	image_uri = "${aws_ecr_repository.lambda_ecr.repository_url}:latest"
 
 
 
