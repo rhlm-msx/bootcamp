b2e8a62e302314f46ff1f448802011caefa79cd8
Rahul Mishra <rahul.mishra@mindstix.com>
2025-09-19 10:24:39 +0000

    added s3 bucket in tf

---
+++ infra/buck.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "assets" {
+}
+
+resource "aws_s3_bucket_acl" "buck_acl" {
+    bucket = aws_s3_bucket.assets.id
+    acl = "private"
+}
+
+output "assets_bucket" {
+    value = aws_s3_bucket.assets.bucket
+}
